e779549356eb25aa6b02d1af176f09b200cd136f
Luis Edymerchk Laverde <edyhack@gmail.com>
2019-08-27 14:40:32 -0500

    basic layout

--- screens/NewScreen.js
+++ screens/NewScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function NewScreen() {
   return (
     <ScrollView style={styles.container}>
+    <Text style={styles.getStartedText}>Take a picture and fill the details!</Text>
+
+    <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
 
     </ScrollView>
   );
@@ -18,5 +30,23 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: '#fff',
+  },
+  getStartedText:{
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
   },
 });
